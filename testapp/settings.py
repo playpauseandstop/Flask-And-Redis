d999a6fd70fdbd2e22981a08ece733b974347f3a
Igor Davydenko <playpauseandstop@gmail.com>
2012-08-15 15:50:17 +0300

    Load local settings for testapp if any.

--- testapp/settings.py
+++ testapp/settings.py
@@ -2,3 +2,14 @@ COUNTER_KEY = 'testapp:counter'
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
+
+
+try:
+    import settings_local
+except ImportError:
+    pass
+else:
+    for attr in dir(settings_local):
+        if attr.startswith('_'):
+            continue
+        locals()[attr] = getattr(settings_local, attr)
