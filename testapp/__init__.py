1894be5d87384e59eccdbcb430abd6b5846dd568
Igor Davydenko <playpauseandstop@gmail.com>
2012-10-23 17:40:10 +0300

    Check that ``redis-py==2.7`` compatible with ``Flask-And-Redis``.

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'Igor Davydenko <playpauseandstop@gmail.com>'
 __description__ = 'Flask-And-Redis test project'
 __license__ = 'BSD License'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
